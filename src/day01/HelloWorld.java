7fa6db81328c24db11b03e1a949f7f1dd32f4fbf
sinemozt <105051040+sinemozt@users.noreply.github.com>
2026-01-19 13:20:03 +0800

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,7 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Sinem");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
 
     }
 }
